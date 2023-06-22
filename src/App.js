f8db9c2fb337a4118985a8a1d06359ee21bd97c1
yashpurkar <109805926+yash-purkar@users.noreply.github.com>
2023-06-22 13:08:05 +0530

    feat: skills page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { About } from "./Components/About";
 import { Home } from "./Components/Home";
 import { Navbar } from "./Components/Navbar";
+import { Skills } from "./Components/Skills";
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Home />
       <About />
+      <Skills />
     </div>
   );
 }
---
+++ src/Components/Skills.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import HTML from '../assets/html.png'
+import CSS from '../assets/css.png'
+import JS from '../assets/javascript.png'
+import ReactImg from '../assets/react.png'
+import TailwindImg from '../assets/tailwind.png'
+import Github from '../assets/github.png'
+
+export const Skills = () => {
+  return (
+    <div name="skills" className='bg-slate-950  text-gray-300 w-full h-screen'>
+
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+
+        <div>
+          <div>
+            <p className='text-4xl font-bold inline border-b-4 border-[#87CEEB]'>Skills</p>
+            <p className='py-4'>Few technologies I've been working with recently</p>
+          </div>
+        </div>
+
+        <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 py-8 text-center'>
+          <div>
+            <img className='w-20 mx-auto' src={HTML} alt="HTML ICON" />
+            <p className='my-4'>HTML</p>
+          </div>
+          <div>
+            <img className='w-20 mx-auto' src={CSS} alt="HTML ICON" />
+            <p className='my-4'>CSS</p>
+          </div>
+          <div>
+            <img className='w-20 mx-auto' src={JS} alt="HTML ICON" />
+            <p className='my-4'>JavaScript</p>
+          </div>
+          <div>
+            <img className='w-20 mx-auto' src={ReactImg} alt="HTML ICON" />
+            <p className='my-4'>React Js</p>
+          </div>
+          <div>
+            <img className='w-20 mx-auto' src={TailwindImg} alt="HTML ICON" />
+            <p className='my-4'>Tailwind CSS</p>
+          </div>
+          <div>
+            <img className='w-20 mx-auto' src={Github} alt="HTML ICON" />
+            <p className='my-4'>GitHub</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
